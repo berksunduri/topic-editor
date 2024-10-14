950e5a326f4f00d6cda92d3aa0a19b24f5538325
Ololade <lolly@gmail.com>
2024-10-14 15:33:40 +0200

    back to app usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.tsx'
+import App from './App.tsx'
 import './index.css'
-import TutorialCheck from './TutorialCheck.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TutorialCheck />
+    <App />
   </StrictMode>,
 )
